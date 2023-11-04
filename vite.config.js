985cd59033bc1b1e429969084544abdac4451d5b
LHufton <lukehufton@gmail.com>
2023-11-04 12:59:41 -0500

    added concurrently

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Log statement to confirm this file is being read
+console.log('Config is being read');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -13,6 +16,7 @@ export default defineConfig({
     https: {
       // https => https://localhost:3000 | http => http://localhost:3000
       maxSessionMemory: 100
-    }
+    },
+    port: 3001  // Set your desired port here
   }
 })
