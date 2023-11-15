1a4d33afdbf36e178c4a96e81e8bd6df814f7c82
LHufton <lukehufton@gmail.com>
2023-11-14 21:08:22 -0600

    server edit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
