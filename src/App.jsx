42d02744c51ca893748ab13923bd7d9c2fe9aa66
LHufton <lukehufton@gmail.com>
2023-11-15 23:07:11 -0600

    text enter update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import './App.css'
 import { useState } from 'react'
 import ForecastList from './components/ForecastList/ForecastList'
-import WeatherDetails from './components/WeatherDetails/WeatherDetails' // Correctly importing WeatherDetails
+import WeatherDetails from './components/WeatherDetails/WeatherDetails'
 
 const App = () => {
   const [city, setCity] = useState('')
   const [inputValue, setInputValue] = useState('')
 
-  const handleCitySearch = () => {
+  const handleCitySearch = (e) => {
+    e.preventDefault() // Prevent the default form submission behavior
     setCity(inputValue)
   }
 
   return (
     <div className="App">
-      <div className="search-bar">
+      <form onSubmit={handleCitySearch} className="search-bar">
         <input
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           placeholder="Enter City"
         />
-        <button onClick={handleCitySearch}>Get Weather</button>
-      </div>
+        <button type="submit">Get Weather</button>
+      </form>
       <WeatherDetails city={city} />
       <ForecastList city={city} />
     </div>
