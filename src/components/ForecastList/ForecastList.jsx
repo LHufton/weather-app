bfe82fa471db97f700910c74de68e9eadaaed5c8
LHufton <lukehufton@gmail.com>
2023-11-17 00:44:35 -0600

    weather icons

--- src/components/ForecastList/ForecastList.jsx
+++ src/components/ForecastList/ForecastList.jsx
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { BASE_URL, API_KEY } from '../../globals'
 import './ForecastList.css'
+import rainyImage from '../../assets/rainy-1.svg'
+import snowyImage from '../../assets/snowy-1.svg'
+import thunderImage from '../../assets/thunder.svg'
+import clearImage from '../../assets/day.svg'
+import defaultImage from '../../assets/weather.svg'
+
+const weatherIcons = {
+  Rain: rainyImage,
+  Snow: snowyImage,
+  Thunderstorm: thunderImage,
+  Clear: clearImage,
+  default: defaultImage
+}
 
 const ForecastList = ({ city }) => {
   const [dailyForecasts, setDailyForecasts] = useState({})
@@ -18,18 +31,18 @@ const ForecastList = ({ city }) => {
             }
           })
 
-          // Aggregate forecasts by day
           const aggregatedByDay = response.data.list.reduce((acc, forecast) => {
             const dateObj = new Date(forecast.dt * 1000)
             const date = dateObj.toLocaleDateString()
             const day = dateObj.toLocaleDateString('en-US', { weekday: 'long' })
-
             const dateString = `${day}, ${date}`
 
             if (!acc[dateString]) {
               acc[dateString] = {
                 min: forecast.main.temp_min,
-                max: forecast.main.temp_max
+                max: forecast.main.temp_max,
+                conditions: [forecast.weather[0].main],
+                clearCondition: forecast.weather[0].main === 'Clear'
               }
             } else {
               acc[dateString].min = Math.min(
@@ -40,9 +53,32 @@ const ForecastList = ({ city }) => {
                 acc[dateString].max,
                 forecast.main.temp_max
               )
+              acc[dateString].conditions.push(forecast.weather[0].main)
+              if (forecast.weather[0].main === 'Clear') {
+                acc[dateString].clearCondition = true
+              }
             }
             return acc
           }, {})
+
+          Object.keys(aggregatedByDay).forEach((date) => {
+            if (aggregatedByDay[date].clearCondition) {
+              aggregatedByDay[date].predominantCondition = 'Clear'
+            } else {
+              const conditionCounts = aggregatedByDay[date].conditions.reduce(
+                (acc, condition) => {
+                  acc[condition] = (acc[condition] || 0) + 1
+                  return acc
+                },
+                {}
+              )
+              aggregatedByDay[date].predominantCondition = Object.keys(
+                conditionCounts
+              ).reduce((a, b) =>
+                conditionCounts[a] > conditionCounts[b] ? a : b
+              )
+            }
+          })
 
           setDailyForecasts(aggregatedByDay)
         } catch (error) {
@@ -57,13 +93,21 @@ const ForecastList = ({ city }) => {
     <div className="forecast">
       <div className="auto-grid-medium">
         <h2>Five Day Forecast</h2>
-        {Object.keys(dailyForecasts).map((dateString) => (
-          <div key={dateString} className="forecast-day">
-            <h3>{dateString}</h3>
-            <p>High: {dailyForecasts[dateString].max.toFixed(2)}°F</p>
-            <p>Low: {dailyForecasts[dateString].min.toFixed(2)}°F</p>
-          </div>
-        ))}
+        {Object.keys(dailyForecasts).map((dateString) => {
+          const dayData = dailyForecasts[dateString]
+          const icon =
+            weatherIcons[dayData.predominantCondition] || weatherIcons.default
+
+          return (
+            <div key={dateString} className="forecast-day">
+              <h3>{dateString}</h3>
+              <img src={icon} alt={dayData.predominantCondition} />
+              <p>High: {dayData.max.toFixed(2)}°F</p>
+              <p>Low: {dayData.min.toFixed(2)}°F</p>
+              <p>Condition: {dayData.predominantCondition}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
--- src/components/WeatherDetails/WeatherDetails.jsx
+++ src/components/WeatherDetails/WeatherDetails.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { BASE_URL, API_KEY } from '../../globals'
+import dayIcon from '../../assets/day.svg'
+import nightIcon from '../../assets/night.svg'
+import rainyImage from '../../assets/rainy-1.svg'
+import snowyImage from '../../assets/snowy-1.svg'
+import thunderImage from '../../assets/thunder.svg'
+import defaultImage from '../../assets/weather.svg'
+
+const weatherImages = {
+  Rain: rainyImage,
+  Snow: snowyImage,
+  Thunderstorm: thunderImage,
+  default: defaultImage
+}
 
 const WeatherDetails = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null)
   const [error, setError] = useState('')
-
-  // Function to convert Kelvin to Fahrenheit
-  const kelvinToFahrenheit = (kelvin) => {
-    return ((kelvin - 273.15) * 9) / 5 + 32
-  }
+  const [weatherImage, setWeatherImage] = useState(defaultImage)
+  const [dayNightImage, setDayNightImage] = useState(dayIcon)
 
   useEffect(() => {
     if (city) {
@@ -19,20 +29,36 @@ const WeatherDetails = ({ city }) => {
             params: {
               q: city,
               appid: API_KEY
-              // Optionally, you can specify units here
-              // units: 'imperial' for Fahrenheit
             }
           })
           setWeatherData(response.data)
+          updateWeatherImage(response.data)
+          updateDayNightImage(response.data)
           setError('')
         } catch (error) {
           setError('Failed to fetch weather data')
-          console.error('Error fetching weather data:', error)
         }
       }
       fetchWeatherData()
     }
   }, [city])
+
+  const kelvinToFahrenheit = (kelvin) => {
+    return ((kelvin - 273.15) * 9) / 5 + 32
+  }
+
+  const updateWeatherImage = (data) => {
+    const mainWeather = data.weather[0].main
+    const image = weatherImages[mainWeather] || weatherImages.default
+    setWeatherImage(image)
+  }
+
+  const updateDayNightImage = (data) => {
+    const currentTime = new Date().getTime() / 1000
+    const isDay =
+      currentTime >= data.sys.sunrise && currentTime < data.sys.sunset
+    setDayNightImage(isDay ? dayIcon : nightIcon)
+  }
 
   return (
     <div>
@@ -40,6 +66,11 @@ const WeatherDetails = ({ city }) => {
       {weatherData && (
         <div>
           <h2>Weather Details for {weatherData.name}</h2>
+          <img src={weatherImage} alt="Weather condition" />
+          <img
+            src={dayNightImage}
+            alt={dayNightImage === dayIcon ? 'Day' : 'Night'}
+          />
           <p>
             Temperature: {kelvinToFahrenheit(weatherData.main.temp).toFixed(2)}
             °F
